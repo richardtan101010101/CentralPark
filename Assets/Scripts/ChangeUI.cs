1694558e30342b79ab4151cf9f971a37bb9a591a
richardtan101010101 <richardtan787@gmail.com>
2020-08-25 17:52:05 -0400

    builtin Game

---
+++ Assets/Scripts/ChangeUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class ChangeUI : MonoBehaviour
+{
+    
+    public Text remainingsnowmens;
+    public int remaning;
+    public void OnTriggerEnter(Collider other)
+    {   
+        Debug.Log("collided");
+        if (other.tag == "Snowman") 
+        {
+            other.gameObject.SetActive(false);
+            Debug.Log("collided");
+            remaning -= 1;
+            Destroy(other);
+        }
+    }
+    void Update()
+    {
+        if (remaning == 0)
+        {
+            SceneManager.LoadScene("Outro");
+        }
+        remainingsnowmens.text = "Remaining Snowmen:" + remaning.ToString();
+    }
+}
